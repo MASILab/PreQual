7ca7ce33597bd8b8eb5aa0242e6c8f888aae1fbe
caily <caily@masi-18.ds.vanderbilt.edu>
2020-08-01 17:02:38 -0500

    Another attempt to get virtual env working for singularity image

--- venv/pip_install_dtiQA.sh
+++ venv/pip_install_dtiQA.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-pip install torch==1.1.0
-pip install torchvision==0.2.2
 pip install nibabel==2.3.0
 pip install scipy==1.1.0
 pip install scikit-learn==0.19.2
+pip install matplotlib==2.2.3
