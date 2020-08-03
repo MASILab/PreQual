b531b63326b78574fe9e9d1ba974c4eea7f4596c
caily <caily@masi-18.ds.vanderbilt.edu>
2020-08-03 13:45:40 -0500

    cleaning up venv install files

--- venv/pip_install_dtiQA.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pip install matplotlib==2.2.3
-pip install scipy==1.1.0
-pip install scikit-learn==0.19.2
-pip install nibabel==2.4.0
--- venv/pip_install_synb0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pip install torch==1.1.0
-pip install torchvision==0.2.2
-pip install nibabel==2.4.0
-pip install scipy==1.1.0
-pip install scikit-learn==0.19.2
